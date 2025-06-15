3567aaa9913b79fda4dc3e0b5bd79dfb6b87a73a
CatastroByte <andrea@catastrobyte.com>
2025-06-15 16:30:35 +0200

    Update Header.vue

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -2,10 +2,10 @@
 <template>
   <v-container fluid class="app-bar">
     <v-row justify="center" align-items="center" align="center">
-      <v-col cols="auto">
+      <v-col cols="6">
         <img alt="CatastroByte Logo" class="logo" src="@/assets/logo.png" />
       </v-col>
-      <v-col cols="auto">
+      <v-col cols="6">
         <h1 class="title">CatastroByte</h1>
         <h4 class="subtitle">When code is a cat-astrophy! 🏆</h4>
       </v-col>
