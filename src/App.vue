20c07d043c28562a6fe7304972971b8a8973f4a4
Ottabit <andrea@ottabit.com>
2025-06-15 14:48:51 +0200

    First commit

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<script setup>
+import { RouterLink, RouterView } from 'vue-router'
+import HelloWorld from './components/HelloWorld.vue'
+import Header from './components/Header.vue'
+</script>
+
+<template>
+  <v-layout class="" height="100vh">
+    <v-app-bar height='130'>
+      <Header />
+    </v-app-bar>
+    <v-main class="d-flex align-center justify-center">
+      <RouterView />
+    </v-main>
+  </v-layout>
+</template>
---
+++ src/components/Header.vue
@@ -0,0 +1,16 @@
+<script setup></script>
+<template>
+  <v-container fluid class="app-bar">
+    <v-row justify="center" align-items="center" align="center">
+      <v-col cols="auto">
+        <img alt="CatastroByte Logo" class="logo" src="@/assets/logo.png" width="125" height="125" />
+      </v-col>
+      <v-col cols="auto">
+        <h1 class="title">CatastroByte</h1>
+        <h4 class="subtitle">When code is a cat-astrophy! 🏆</h4>
+      </v-col>
+    </v-row>
+  </v-container>
+  <div>
+  </div>
+</template>
