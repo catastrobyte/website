224ad5bb4d5956592ac8d111e6964b159128dd40
Ottabit <andrea@ottabit.com>
2025-06-21 18:21:20 +0200

    Update Homepage

--- src/views/Homepage.vue
+++ src/views/Homepage.vue
@@ -3,15 +3,16 @@ import catPaw from "../assets/cat_paw.png"
 import catKeyboard from "../assets/cat_keyboard.png"
 import catSpaceship from "../assets/cat_spaceship.png"
 import catExplosion from "../assets/cat_explosion.png"
+import blackHole from "../assets/black_hole.png"
 
 </script>
 <template>
   <v-container fluid>
     <v-row justify="center" align-items="center" align="center">
       <v-col cols="auto" xs="12">
-        <v-card color="secondary" rounded="lg">
+        <v-card color="secondary" rounded="lg" min-width="320" max-width="320">
           <v-card-item>
-            <p>
+            <p class="text-center">
               <i>
                 A common catastrophic day is made of...
               </i>
@@ -60,7 +61,23 @@ import catExplosion from "../assets/cat_explosion.png"
             <v-img :src="catExplosion" width="125" />
           </v-card-item>
           <v-card-item class="text-center pt-0">
-            <p>Enjoy the catastrophy!</p>
+            <p>Enjoy the catastrophe!</p>
+          </v-card-item>
+        </v-card>
+      </v-col>
+    </v-row>
+    <v-row justify="center" align="center" align-items="center">
+      <v-col class="d-flex justify-center">
+        <v-card rounded="lg" min-width="320" max-width="320" color="secondary">
+          <v-card-title>
+            Hey, folks!
+          </v-card-title>
+          <v-card-item class="d-flex justify-center">
+            <v-img :src="blackHole" cover width="128" />
+          </v-card-item>
+          <v-card-item>
+            <p>Probably whoever created life never wrote tests and always tested in production.</p>
+            <p>If this is true, I am a creator too! 😼</p>
           </v-card-item>
         </v-card>
       </v-col>
