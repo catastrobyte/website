baa2c092fb04a29a962b6543b0f506d4c039f32e
Ottabit <andrea@ottabit.com>
2025-06-22 14:28:11 +0200

    Fixed index.js file

--- src/index.js
+++ src/index.js
@@ -1,5 +1,26 @@
-export default {
-  fetch() {
-    return new Response(`Running in ${navigator.userAgent}!`);
-  },
-};
+addEventListener('fetch', event => {
+  event.respondWith(handleRequest(event.request));
+});
+
+async function handleRequest(request) {
+  const url = new URL(request.url);
+
+  const filePath = url.pathname;
+
+  const staticFileExtensions = [
+    '.html', '.css', '.js', '.json', '.png', '.jpg', '.jpeg', '.gif', '.svg',
+    '.ico', '.woff', '.woff2', '.ttf', '.eot', '.map'
+  ];
+
+  const isStaticFile = staticFileExtensions.some(ext => filePath.endsWith(ext));
+
+  if (!isStaticFile && filePath !== '/') {
+    url.pathname = '/index.html'; // Puntiamo sempre a index.html
+    return fetch(url.toString());
+  } else if (filePath === '/') {
+    url.pathname = '/index.html';
+    return fetch(url.toString());
+  } else {
+    return fetch(request);
+  }
+}
