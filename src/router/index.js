c45e447c517fef86fcb335d55b48340722941dea
Ottabit <andrea@ottabit.com>
2025-08-13 01:36:26 +0200

    Fix route

--- src/router/index.js
+++ src/router/index.js
@@ -1,4 +1,4 @@
-import Homepage from '@/views/Homepage.vue'
+import HomePage from '@/views/HomePage.vue'
 import { createRouter, createWebHistory } from 'vue-router'
 
 const router = createRouter({
@@ -7,7 +7,7 @@ const router = createRouter({
     {
       name: 'homepage',
       path: '/',
-      component: Homepage,
+      component: HomePage,
     },
   ],
 })
